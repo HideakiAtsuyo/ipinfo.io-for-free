bc471a428ecc7a867e7832ebe3ec8edaf4217562
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-09-29 01:54:14 +0200

    Parse Example

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using Leaf.xNet;
 using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace IpInfoDotIoForFree
 {
@@ -9,11 +11,11 @@ namespace IpInfoDotIoForFree
         {
             Console.Write("IP: ");
             string ip = Console.ReadLine();
-
+            Console.Clear();
             using (HttpRequest req = new HttpRequest())
             {
                 req.IgnoreProtocolErrors = true;
-                req.Proxy = HttpProxyClient.Parse("209.141.62.66:1337"); //Use Proxy To Avoid The Rate Limit(put a proxy list don't be dumb huh :()
+                req.Proxy = HttpProxyClient.Parse("190.107.224.150:3128"); //Use Proxy To Avoid The Rate Limit(put a proxy list don't be dumb huh :()
                 req.AddHeader("Referer", "https://ipinfo.io");
                 req.AddHeader("User-Agent", "HelloWorld");
 
@@ -21,7 +23,11 @@ namespace IpInfoDotIoForFree
                 if (res.StatusCode == HttpStatusCode.TooManyRequests)
                     Console.WriteLine("Rate Limited");
                 else
-                    Console.WriteLine(res.ToString());
+                {
+                    JObject json = JObject.Parse(res.ToString());
+                    Console.WriteLine(String.Format("Checked IP: {0}\nASN Name: {1}\nCompany Name: {2}\nPrivacy: VPN: {3} | Proxy: {4} | Tor: {5} | Hosting: {6}\nAbuse Email: {7}\nDomains Count: {8}", json["ip"], json["asn"]["name"], json["company"]["name"], json["privacy"]["vpn"], json["privacy"]["proxy"], json["privacy"]["tor"], json["privacy"]["hosting"], json["abuse"]["email"], json["domains"]["total"]));
+                }
+                    
                 req.Close();
             }
             Console.ReadLine();
